c4877e273465637df280c0b67c73f177803baef8
Tim Visée <timvisee@gmail.com>
2018-02-14 14:09:54 +0100

    Put host configuration in application configuration file

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -11,9 +11,19 @@ namespace webapp
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+            // Get the host from the configuration
+            AppConfig config = new AppConfig();
+
+            // Get the host
+            String host = config.GetProperty("Web.Hosts");
+            if (host == null) {
+                throw new Exception("Missing 'Web.Hosts' key in configuration, unable to host");
+            }
+
+            // Buuld the web host
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://*:5000")
+                .UseUrls(host)
                 .Build();
     }
 }
