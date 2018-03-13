82039cb1c9d5becfc58ffe5146cd0f2549c52b6a
Tim Visée <timvisee@gmail.com>
2018-03-13 11:35:20 +0100

    Create people user getter, groups list people not users

--- backend/Models/Group.cs
+++ backend/Models/Group.cs
@@ -5,15 +5,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace backend.Models
 {
     [Table("groups")]
-    public class Group
+    public class Group : People
     {
         [Key]
         [ForeignKey("People")]
         public int PeopleId { get; set; }
-        
+
         public string Name { get; set; }
 
-        [ForeignKey("User")]
-        public List<User> Users { get; set; }
+        [ForeignKey("People")]
+        public List<People> Peoples { get; set; }
     }
 }
