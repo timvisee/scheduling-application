e515286032b54353294e4225fee1d28483f9e273
Dennis Volkering <dennisvolkering@hotmail.com>
2018-03-14 11:10:26 +0100

    Scaffolded all models

---
+++ backend/Controllers/PeopleController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PeopleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: People
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Peoples.ToListAsync());
+        }
+
+        // GET: People/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var people = await _context.Peoples
+                .SingleOrDefaultAsync(m => m.PeopleId == id);
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            return View(people);
+        }
+
+        // GET: People/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PeopleId")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(people);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(people);
+        }
+
+        // GET: People/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var people = await _context.Peoples.SingleOrDefaultAsync(m => m.PeopleId == id);
+            if (people == null)
+            {
+                return NotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PeopleId")] People people)
+        {
+            if (id != people.PeopleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(people);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PeopleExists(people.PeopleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(people);
+        }
+
+        // GET: People/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var people = await _context.Peoples
+                .SingleOrDefaultAsync(m => m.PeopleId == id);
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            return View(people);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var people = await _context.Peoples.SingleOrDefaultAsync(m => m.PeopleId == id);
+            _context.Peoples.Remove(people);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PeopleExists(int id)
+        {
+            return _context.Peoples.Any(e => e.PeopleId == id);
+        }
+    }
+}
