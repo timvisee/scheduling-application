fe142e8aed2afac7b45a3b0ca34f3fa8c41d2080
fleur <12059315@student.hhs.nl>
2018-03-22 15:34:11 +0100

    Created reservation

---
+++ backend/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Models
+{
+    [Table("reservation")]
+    public class Reservation
+
+        {
+            [Key]
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+
+            [ForeignKey("People")]
+            public List<People> Peoples { get; set; }
+
+            public ICollection <EventLocation> Locations { get; set; }
+        }
+    }
+}
