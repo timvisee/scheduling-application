d713355ed85b772f1b5b2e9752cb1b60626e909e
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2018-03-29 12:50:04 +0200

    Show message when event description is missing

--- backend/wwwroot/js/site.js
+++ backend/wwwroot/js/site.js
@@ -3,7 +3,14 @@
         axios.get('/events/jsondetails/' + $(this).data('id'))
         .then(function (response) {
             $("#detail-title").text(response.data.event.title);
-            $("#detail-description").text(response.data.event.description);
+
+
+            if (response.data.event.description !== null) {
+                $("#detail-description").text(response.data.event.description);
+            } else {
+                $("#detail-description").text("No description available");
+            }
+
             $("#detail-time-duration").text(response.data.timeDuration);
             $("#detail-start-date").text(response.data.startDate);
             $("#detail-end-date").text(response.data.endDate);
